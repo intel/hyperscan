d9efe071254acd762f12598f16c4a76a71082288
Justin Viiret <justin.viiret@intel.com>
2015-11-03 16:24:06 +1100

    depth: correct sign in printf format

--- src/util/depth.h
+++ src/util/depth.h
@@ -183,7 +183,7 @@ public:
 
         s64a rv = val + d;
         if (rv < 0 || (u64a)rv >= val_infinity) {
-            DEBUG_PRINTF("depth %llu too large to represent!\n", rv);
+            DEBUG_PRINTF("depth %lld too large to represent!\n", rv);
             throw DepthOverflowError();
         }
 
