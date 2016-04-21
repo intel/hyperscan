fe8ffc55445fd570565f50d2f59f42cca455e7e1
Matthew Barr <matthew.barr@intel.com>
2016-04-21 15:39:47 +1000

    noodle: use SSE palignr

--- src/hwlm/noodle_engine.c
+++ src/hwlm/noodle_engine.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Intel Corporation
+ * Copyright (c) 2015-2016, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -37,6 +37,7 @@
 #include "util/compare.h"
 #include "util/masked_move.h"
 #include "util/simd_utils.h"
+#include "util/simd_utils_ssse3.h"
 
 #include <ctype.h>
 #include <stdbool.h>
