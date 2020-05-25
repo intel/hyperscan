020a219505c27c52a6358b971e6e45fb5266aaae
Hong, Yang A <yang.a.hong@intel.com>
2020-05-25 14:03:41 +0000

    Update for Hyperscan 5.3.0

--- dev-reference/_static/documentation_options.js
+++ dev-reference/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.2.1',
+    VERSION: '5.3.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
